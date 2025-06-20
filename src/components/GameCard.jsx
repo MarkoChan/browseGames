1351516fe911a4980321a31436fbb1994076c8f2
MarkoChan <chan.marko112@gmail.com>
2025-06-20 16:37:56 -0600

    added sidebar component and adjusted background

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,7 +1,7 @@
 import './GameCard.css'
 import { useState } from 'react';
 
-function GameCard({game, scale = 1}){
+function GameCard({ game, scale = 1 }) {
     // init gamecard state
     const [wishlisted, setWishlisted] = useState(game.wishlisted);
     const cardStyle = {
@@ -21,15 +21,15 @@ function GameCard({game, scale = 1}){
     }
 
     return (
-        <div className="game_card" onClick={onCardClick} style={cardStyle}>
-            <div className="game_cover">
-                <img src={game.cover} alt={game.title}/>
+        <div className="game-card" onClick={onCardClick} style={cardStyle}>
+            <div className="game-cover">
+                <img src={game.cover} alt={game.title} />
             </div>
-            <div className="game_overlay">
+            <div className="game-overlay">
                 <button
-                className={`wishlist_icon_btn ${wishlisted === 1 ? 'wishlisted' : ''}`}
-                onClick={onWishlistClick}
-                aria-label="Toggle wishlist"
+                    className={`wishlist-icon-btn ${wishlisted === 1 ? 'wishlisted' : ''}`}
+                    onClick={onWishlistClick}
+                    aria-label="Toggle wishlist"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -42,18 +42,18 @@ function GameCard({game, scale = 1}){
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42
                                 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81
                                 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4
-                                6.86-8.55 11.54L12 21.35z" 
+                                6.86-8.55 11.54L12 21.35z"
                         />
                     </svg>
                 </button>
             </div>
 
-            <div className="game_info" style={{ color: 'white' }}>
+            <div className="game-info" style={{ color: 'white' }}>
                 <h3>{game.title}</h3>
                 <p>{game.price}</p>
             </div>
         </div>
-    )
+    );
 }
 
-export default GameCard
+export default GameCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import GameCard from '../components/GameCard';
 import Carousel from '../components/carousel';
 import GameBanner from '../components/GameBanner';
+import VerticalSidebar from '../components/VerticalSidebar'
 import '../styles/Global.css';
 import '../styles/Home.css'
 
 
 export default function Home() {
+    // demo banners
     const gameBanners = [
     <GameBanner game={{ cover: "src/assets/E33banner.png", title: "Clair Obscur: Expedition 33", price: "$2.50", wishlisted: 0 }} />,
     <GameBanner game={{ cover: "src/assets/ERNRbanner.png", title: "Elden Ring: Nightreign", price: "$2.50", wishlisted: 0 }} />,
@@ -13,72 +15,83 @@ export default function Home() {
     <GameBanner game={{ cover: "src/assets/SDVbanner.png", title: "Stardew Valley", price: "$2.50", wishlisted: 0 }} />,
     <GameBanner game={{ cover: "src/assets/hadesBanner.png", title: "Hades", price: "$2.50", wishlisted: 0 }} />
     ];
+
+    // demo genres
+    const genres = ["Action", "Adventure", "RPG", "Shooter", "Strategy", "Simulation", "Sports", "Racing", "Puzzle", "Platformer", "Fighting", "Horror", "MMO", "Sandbox", "Stealth", "Survival", "Rhythm", "Card Game", "Turn-Based", "Roguelike"];
+
     return (
         <div className="page-wrapper">
-            {/* Search Bar */}
-            <div style={{display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
-                <input className='searchInput' type="text" placeholder="Search..." />
-                <button className='searchBtn'>Search</button>
-            </div>
-            <br></br>
-            {/* Game banner Carousel */}
-            <div className="centered-wrapper">
-               <Carousel
-                    items={gameBanners}
-                    visibleCount={1}
-                    itemWidth={1280}
-                    itemHeight={700}
-                    autoScroll={true}
-                    scrollInterval={4000}
-                />
-            </div>
-            {/* Trending games */}
-            <div className="centered-wrapper">
-                <h1 className="offset-box">Trending</h1>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'center', transform: 'scale(0.9) translateY(-50px)'}}>
-                <table>
-                    <tr>
-                    <th><GameCard game={{ cover: "src/assets/honse.png", title: "Honse 2", price: "$2.50", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/cowboy.png", title: "Cowboy Man", price: "$21.50", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    </tr>
-                    <tr>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    </tr>
-                </table>
-            </div>
-        {/* Discounted games */}
-        <div className="centered-wrapper">
-            <h1 className="offset-box">Discounted</h1>
-        </div>
-        <div style={{display: 'flex', justifyContent: 'center', transform: 'scale(0.9) translateY(-50px)'}}>
-                <table>
-                    <tr>
-                    <th><GameCard game={{ cover: "src/assets/honse.png", title: "Honse 2", price: "$2.50", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/cowboy.png", title: "Cowboy Man", price: "$21.50", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    </tr>
-                    <tr>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
-                    </tr>
-                </table>
+            <div className='layout'>
+                <div className='sidebar'>
+                    <VerticalSidebar text_list={genres}/>
+                </div>
+                <div className='main'>
+                    {/* Search Bar */}
+                    <div style={{display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
+                        <input className='searchInput' type="text" placeholder="Search..." />
+                        <button className='searchBtn'>Search</button>
+                    </div>
+                    <br></br>
+                    {/* Game banner Carousel */}
+                    <div className="centered-wrapper">
+                    <Carousel
+                            items={gameBanners}
+                            visibleCount={1}
+                            itemWidth={1280}
+                            itemHeight={700}
+                            autoScroll={true}
+                            scrollInterval={4000}
+                        />
+                    </div>
+                    {/* Trending games */}
+                    <div className="centered-wrapper">
+                        <h1 className="offset-box">Trending</h1>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'center', transform: 'scale(0.9) translateY(-50px)'}}>
+                        <table>
+                            <tr>
+                            <th><GameCard game={{ cover: "src/assets/honse.png", title: "Honse 2", price: "$2.50", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/cowboy.png", title: "Cowboy Man", price: "$21.50", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            </tr>
+                            <tr>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            </tr>
+                        </table>
+                    </div>
+                    {/* Discounted games */}
+                    <div className="centered-wrapper">
+                        <h1 className="offset-box">Discounted</h1>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'center', transform: 'scale(0.9) translateY(-50px)'}}>
+                        <table>
+                            <tr>
+                            <th><GameCard game={{ cover: "src/assets/honse.png", title: "Honse 2", price: "$2.50", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/cowboy.png", title: "Cowboy Man", price: "$21.50", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            </tr>
+                            <tr>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            <th><GameCard game={{ cover: "src/assets/_missing.png", title: "N/A", price: "$NAN", wishlisted: 0 }} /></th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     );
