ae1fc576ff59349f72679878bf8aa8b02c06a69b
Albert Huang <albert7@ualberta.ca>
2025-06-22 18:30:53 -0600

    Added API stuff. Need to make it work with main branch

--- server/server.js
+++ server/server.js
@@ -3,11 +3,42 @@ import cors from 'cors';
 
 const app = express();
 const PORT = 3001;
+const RAWG_API_KEY = 'c769e5820efe4949b6fc829fdc8066ad'
 
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static('server/images'));
+app.get('/api/games', async (req, res) => {
+    try {
+        // Make the http request
+        const rawgReq = await fetch(`https://api.rawg.io/api/games?key=${RAWG_API_KEY}&page_size=10`);
 
+        // wait for response
+        const data = await rawgReq.json();
+
+        // debug
+        console.log("RAWG API response:", data);
+        
+        // Convert API data to match ours
+        const games = data.results.map(game => ({
+            id: game.id,
+            title: game.name,
+            thumbnail: game.background_image,
+            price: "$0.00",  // no price from RAWG api?
+            wishlisted: 0
+        }))
+
+        res.json(games)
+    } catch (err) {
+        console.log("Failed to get RAWG data, error: ", err);
+        res.status(500).json({ error: " Failed to fetch games "});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
+
+/*
 // Store Game Data and stuff here. For now hard code an array, but switch to a database later
 const games = [
     {
@@ -33,3 +64,4 @@ app.get('/api/games', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
+*/
